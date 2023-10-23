71d5e98e00e72e865ea15098d23c7e43799bc287
Joben <ven.wbridges@gmail.com>
2023-10-23 19:08:46 +0800

    added function

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,14 +17,13 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession();
-
   return (
     <html lang="en">
       {/* <script src="https://cdn.tailwindcss.com"></script> */}
       <body className={inter.className}>
         <Providers>
           {" "}
-          <SessionProvider session={session}> {children}</SessionProvider>
+          <SessionProvider session={session}>{children}</SessionProvider>
         </Providers>
       </body>
     </html>
--- frontend/components/Cms/Cms.tsx
+++ frontend/components/Cms/Cms.tsx
@@ -1,13 +1,15 @@
-import Navbar from "./Navbar"
-import Content from "./Content"
+"use client";
+import Navbar from "./Navbar";
+import Content from "./Content";
 
 function index() {
+  console.log(window.location.pathname);
   return (
-    <div className=' flex w-full h-full '>
-      <Navbar/>
-      <Content/>
+    <div className=" flex w-full h-full ">
+      <Navbar />
+      <Content />
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
